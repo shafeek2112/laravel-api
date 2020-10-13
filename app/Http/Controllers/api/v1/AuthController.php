d2169756e05e17436e00ba1b9ac484b5c96d3b92
Shafeek <mohammedshfeek2112@gmail.com>
2020-10-13 22:47:18 +0800

    first commit

---
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Passport\Passport;
+use Carbon\Carbon;
+
+class AuthController extends Controller
+{
+    use ApiResponser;
+
+    /**
+    * Create user
+    *
+    * @param  [string] name
+    * @param  [string] email
+    * @param  [string] password
+    * @param  [string] password_confirmation
+    * @return [string] message
+    */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        $user->save();
+
+        return $this->success('User Created Successfully', 201);
+    }
+
+    /**
+     * Login user and create token
+     *
+     * @param  [string] email
+     * @param  [string] password
+     * @param  [boolean] remember_me
+     * @return [string] access_token
+     * @return [string] token_type
+     * @return [string] expires_at
+     */
+    public function login(Request $request)
+    {
+        $attr = $request->validate([
+            'email'         => 'required|string|email',
+            'password'      => 'required|string',
+            'remember_me'   => 'boolean'
+        ]);
+
+        if (!Auth::attempt($attr)) {
+            return $this->error('Credentials mismatch', 401);
+        }
+
+        //Genereate the token and save into DB
+        $user = $request->user();
+        $tokenResult = $user->createToken('AspireAPIAccessToken');
+        $token = $tokenResult->token;
+
+        //Check rememberme
+        if ($request->remember_me)
+            $token->expires_at = Carbon::now()->addWeeks(1);
+
+        $token->save(); 
+
+        return $this->token($token);
+    }
+
+    /**
+     * Logout user (Revoke the token)
+     *
+     * @return [string] message
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+        return $this->success('User Logged Out', 200);
+    }
+
+    /**
+     * Get the authenticated User
+     *
+     * @return [json] user object
+     */
+    public function user(Request $request)
+    {
+        return response()->json($request->user());
+    }
+}
